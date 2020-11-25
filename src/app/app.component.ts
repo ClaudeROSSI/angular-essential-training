be872c4dcc4e2c43f2205fbdbdf3461f54a77baa
Claude ROSSI <claude.rossi@sap.com>
2020-11-25 18:14:51 +0100

    CR - templateUrl / styleUrls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<h1>Ng Calude</h1>'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
