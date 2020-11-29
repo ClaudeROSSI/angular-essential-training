19ccf960aaec47c37c78cb0f8e47961ace7e6e14
Claude ROSSI <claude.rossi@sap.com>
2020-11-29 12:36:37 +0100

    lazy module loading

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { MediaItemListComponent } from './media-item-list.component';
 
 const appRoutes: Routes = [
+  {
+    path: 'add',
+    loadChildren: () => {
+      return import('./new-item/new-item.module').then( childModule => {
+          return childModule.NewItemModule;
+        }
+      )
+    }
+  },
   { path: ':medium', component: MediaItemListComponent },
   { path: '', pathMatch: 'full', redirectTo: 'all' }
 ];
--- src/app/new-item/new-item.routing.ts
+++ src/app/new-item/new-item.routing.ts
@@ -2,7 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MediaItemFormComponent } from './media-item-form.component';
 
 const newItemRoutes: Routes = [
-  { path: 'add', component: MediaItemFormComponent }
+  {
+    // Lazy loading - /add is now part of the  parent router!!!
+    // path: 'add',
+    path: '',
+    component: MediaItemFormComponent
+  }
 ];
 
 export const newItemRouting = RouterModule.forChild(newItemRoutes);
